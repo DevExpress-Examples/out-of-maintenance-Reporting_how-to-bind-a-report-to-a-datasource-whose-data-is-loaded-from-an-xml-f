6f199dd95d3956a6c847e04a838c6cf24d2587bf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/XtraReport_RuntimeDataBinding/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraReports.UI;
+// ...
+
+namespace XtraReport_RuntimeDataBinding {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create an empty report.
+            XtraReport report = new XtraReport();
+
+            // Create a data source.           
+            SqlDataSource ds = new SqlDataSource(new XmlFileConnectionParameters("../../Cars.xml"));
+
+            // Create a query to access fields of the Cars data table.
+            TableQuery query = new TableQuery();
+            query.Name = "Cars";
+            query.AddTable("Cars")
+                 .SelectColumns("ID", "Trademark", "Model", "Category", "Description");
+            ds.Queries.Add(query);
+
+            // Rebuild the data source schema to reflect the defined data hierarchy.
+            ds.RebuildResultSchema();
+
+            // Assign the data source to the report.
+            report.DataSource = ds;
+            report.DataMember = "Cars";
+
+            // Add a detail band to the report.
+            DetailBand detailBand = new DetailBand();
+            detailBand.Height = 50;
+            report.Bands.Add(detailBand);
+
+            // Add a label to the detail band.
+            XRLabel label = new XRLabel { WidthF = 300 };
+            label.Text = "[Cars.Trademark] [Cars.Model]";
+            detailBand.Controls.Add(label);
+            
+            // Show the report's print preview.
+            report.ShowPreview();       
+        }
+    }
+}
---
+++ VB/XtraReport_RuntimeDataBinding/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.XtraReports.UI
+' ...
+
+Namespace XtraReport_RuntimeDataBinding
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            ' Create an empty report.
+            Dim report As New XtraReport()
+
+            ' Create a data source.           
+            Dim ds As New SqlDataSource(New XmlFileConnectionParameters("../../Cars.xml"))
+
+            ' Create a query to access fields of the Cars data table.
+            Dim query As New TableQuery()
+            query.Name = "Cars"
+            query.AddTable("Cars").SelectColumns("ID", "Trademark", "Model", "Category", "Description")
+            ds.Queries.Add(query)
+
+            ' Rebuild the data source schema to reflect the defined data hierarchy.
+            ds.RebuildResultSchema()
+
+            ' Assign the data source to the report.
+            report.DataSource = ds
+            report.DataMember = "Cars"
+
+            ' Add a detail band to the report.
+            Dim detailBand As New DetailBand()
+            detailBand.Height = 50
+            report.Bands.Add(detailBand)
+
+            ' Add a label to the detail band.
+            Dim label As XRLabel = New XRLabel With {.WidthF = 300}
+            label.Text = "[Cars.Trademark] [Cars.Model]"
+            detailBand.Controls.Add(label)
+
+            ' Show the report's print preview.
+            report.ShowPreview()
+        End Sub
+    End Class
+End Namespace
